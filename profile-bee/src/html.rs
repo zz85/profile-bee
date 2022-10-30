1e73a71fed042a2f21675425aaeeec20050df198
Joshua Koo <zz85nus@gmail.com>
2022-10-30 09:10:11 +0000

    Clean up html module
    - Changed Arc -> Rc
    - Update to html to latest version of http://martinspier.io/d3-flame-graph/
    - option to add values to all or just leave nodes (self_value option)

--- profile-bee/src/html.rs
+++ profile-bee/src/html.rs
@@ -2,21 +2,30 @@
 /// profile stacktraces in an interactive flamegraph format
 use serde::{Deserialize, Serialize};
 use serde_json;
-use std::{cell::RefCell, path::Path, sync::Arc};
+use std::{cell::RefCell, path::Path, rc::Rc};
 
 /// hierarchical data structure
 /// in the form of { name, value, children }
 #[derive(Serialize, Deserialize, Default)]
-struct Stack {
-    name: String,
+struct Stack<'a> {
+    name: &'a str,
     value: usize,
-    children: Vec<Arc<RefCell<Stack>>>,
+    children: Vec<Rc<RefCell<Stack<'a>>>>,
+}
+
+impl<'a> Stack<'a> {
+    fn new(name: &'a str) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Self {
+            name,
+            ..Default::default()
+        }))
+    }
 }
 
 /// turns a sorted stackcollapsed format into d3-flamegraph json format
 pub fn collapse_to_json(stacks: &[&str]) -> String {
-    let root = Arc::new(RefCell::new(Stack::default()));
-    let mut crumbs: Vec<Arc<RefCell<Stack>>> = vec![root.clone()];
+    let root = Stack::new("");
+    let mut crumbs = vec![root.clone()];
 
     for stack in stacks {
         let mut parts = stack.split(' ');
@@ -35,18 +44,10 @@ pub fn collapse_to_json(stacks: &[&str]) -> String {
                 // new flow
                 crumbs.truncate(depth);
 
-                let mut node = Stack::default();
-                node.name = name.to_owned();
+                let node = Stack::new(name);
 
-                let anode = Arc::new(RefCell::new(node));
-
-                let last = crumbs
-                    .last()
-                    .unwrap_or_else(|| unreachable!("last entry in stacks"));
-
-                last.borrow_mut().children.push(anode.clone());
-
-                crumbs.push(anode);
+                crumbs[depth - 1].borrow_mut().children.push(node.clone());
+                crumbs.push(node);
             }
         }
 
@@ -54,8 +55,20 @@ pub fn collapse_to_json(stacks: &[&str]) -> String {
             crumbs.truncate(depth);
         }
 
-        for node in crumbs.iter() {
-            node.borrow_mut().value += count;
+        let self_value = true;
+        if self_value {
+            // if we were to use selfValue(true), this inserts values only
+            // at leave nodes
+            crumbs
+                .last()
+                .unwrap_or_else(|| unreachable!("always have one"))
+                .borrow_mut()
+                .value += count;
+        } else {
+            // adds count to all nodes along the path
+            for node in crumbs.iter() {
+                node.borrow_mut().value += count;
+            }
         }
     }
 
@@ -80,16 +93,16 @@ fn test_serialization() {
     );
 
     let mut test = Stack::default();
-    test.name = "hi".to_string();
+    test.name = "hi";
     test.value = 10;
     let mut test1 = Stack::default();
-    test1.name = "test 1".to_string();
+    test1.name = "test 1";
     test1.value = 3;
     let mut test2 = Stack::default();
-    test2.name = "test 2".to_string();
+    test2.name = "test 2";
     test2.value = 4;
-    test.children.push(Arc::new(RefCell::new(test1)));
-    test.children.push(Arc::new(RefCell::new(test2)));
+    test.children.push(Rc::new(RefCell::new(test1)));
+    test.children.push(Rc::new(RefCell::new(test2)));
 
     let test_json = serde_json::to_string(&test).expect("serialization to json");
 
@@ -111,15 +124,20 @@ const HTML_TEMPLATE: &str = r##"
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/spiermar/d3-flame-graph@1.0.4/dist/d3.flameGraph.min.css">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/spiermar/d3-flame-graph@2.0.3/dist/d3-flamegraph.css">
+
     <style>
+
     /* Space out content a bit */
     body {
       padding-top: 20px;
       padding-bottom: 20px;
     }
+
     /* Custom page header */
     .header {
       padding-bottom: 20px;
@@ -127,12 +145,14 @@ const HTML_TEMPLATE: &str = r##"
       padding-left: 15px;
       border-bottom: 1px solid #e5e5e5;
     }
+
     /* Make the masthead heading the same height as the navigation */
     .header h3 {
       margin-top: 0;
       margin-bottom: 0;
       line-height: 40px;
     }
+
     /* Customize container */
     .container {
       max-width: 990px;
@@ -168,46 +188,87 @@ const HTML_TEMPLATE: &str = r##"
       <div id="details">
       </div>
     </div>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/4.10.0/d3.min.js"></script>
-      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3-tip/0.7.1/d3-tip.min.js"></script>
-      <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/spiermar/d3-flame-graph@1.0.4/dist/d3.flameGraph.min.js"></script>
+
+    <!-- D3.js -->
+    <script src="https://d3js.org/d3.v4.min.js" charset="utf-8"></script>
+
+    <!-- d3-tip -->
+    <script type="text/javascript" src=https://cdnjs.cloudflare.com/ajax/libs/d3-tip/0.9.1/d3-tip.min.js></script>
+
+    <!-- d3-flamegraph -->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/spiermar/d3-flame-graph@2.0.3/dist/d3-flamegraph.min.js"></script>
+
     <script type="text/javascript">
-        var data = {stack};
+      var data = {stack};
     </script>
+
     <script type="text/javascript">
-    var flameGraph = d3.flameGraph()
+    var flameGraph = d3.flamegraph()
       .width(960)
       .cellHeight(18)
       .transitionDuration(750)
+      .minFrameSize(5)
       .transitionEase(d3.easeCubic)
-      .sort(!true)
+      .sort(true)
+      //Example to sort in reverse order
+      //.sort(function(a,b){ return d3.descending(a.name, b.name);})
       .title("")
-      .onClick(onClick);
+      .onClick(onClick)
+      .differential(false)
+      .selfValue(!false);
+
+
     // Example on how to use custom tooltips using d3-tip.
-    var tip = d3.tip()
-      .direction("s")
-      .offset([8, 0])
-      .attr('class', 'd3-flame-graph-tip')
-      .html(function(d) { return "name: " + d.data.name + ", value: " + d.data.value; });
-    flameGraph.tooltip(tip);
+    // var tip = d3.tip()
+    //   .direction("s")
+    //   .offset([8, 0])
+    //   .attr('class', 'd3-flame-graph-tip')
+    //   .html(function(d) { return "name: " + d.data.name + ", value: " + d.data.value; });
+
+    // flameGraph.tooltip(tip);
+
     d3.select("#chart")
       .datum(data)
       .call(flameGraph);
+
+    var details = document.getElementById("details");
+    flameGraph.setDetailsElement(details);
+
+    // Example on how to use custom labels
+    // var label = function(d) {
+    //  return "name: " + d.name + ", value: " + d.value;
+    // }
+    // flameGraph.label(label);
+
+    // Example of how to set fixed chart height
+    // flameGraph.height(540);
+
+    d3.json("stacks.json", function(error, data) {
+      if (error) return console.warn(error);
+      d3.select("#chart")
+          .datum(data)
+          .call(flameGraph);
+    });
+
     document.getElementById("form").addEventListener("submit", function(event){
       event.preventDefault();
       search();
     });
+
     function search() {
       var term = document.getElementById("term").value;
       flameGraph.search(term);
     }
+
     function clear() {
       document.getElementById('term').value = '';
       flameGraph.clear();
     }
+
     function resetZoom() {
       flameGraph.resetZoom();
     }
+
     function onClick(d) {
       console.info("Clicked on " + d.data.name);
     }
